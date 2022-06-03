9d99355af2ae66c711cdf06557e29ba922872155
budowskiii <106811443+budowskiii@users.noreply.github.com>
2022-06-03 16:15:54 +0500

    Update gps-speedometer.ino
    The function called to assign the gps coordinates, speed the satellite number and altitude.

--- gps-speedometer/gps-speedometer.ino
+++ gps-speedometer/gps-speedometer.ino
@@ -1,23 +1,42 @@
-void loop() {
+void print_speed()
+{
+  display.clearDisplay();
+  display.setTextColor(SSD1306_WHITE);
+       
+  if (gps.location.isValid() == 1)
+  {
+   //String gps_speed = String(gps.speed.kmph());
+    display.setTextSize(1);
     
-  boolean newData = false;
-  for (unsigned long start = millis(); millis() - start < 1000;)
-  {
-    while (neogps.available())
-    {
-      if (gps.encode(neogps.read()))
-      {
-        newData = true;
-      }
-    }
-  }
+    display.setCursor(25, 5);
+    display.print("Lat: ");
+    display.setCursor(50, 5);
+    display.print(gps.location.lat(),6);
 
-  //If newData is true
-  if(newData == true)
-  {
-    newData = false;
-    Serial.println(gps.satellites.value());
-    print_speed();
+    display.setCursor(25, 20);
+    display.print("Lng: ");
+    display.setCursor(50, 20);
+    display.print(gps.location.lng(),6);
+
+    display.setCursor(25, 35);
+    display.print("Speed: ");
+    display.setCursor(65, 35);
+    display.print(gps.speed.kmph());
+    
+    display.setTextSize(1);
+    display.setCursor(0, 50);
+    display.print("SAT:");
+    display.setCursor(25, 50);
+    display.print(gps.satellites.value());
+
+    display.setTextSize(1);
+    display.setCursor(70, 50);
+    display.print("ALT:");
+    display.setCursor(95, 50);
+    display.print(gps.altitude.meters(), 0);
+
+    display.display();
+    
   }
   else
   {
@@ -28,5 +47,5 @@ void loop() {
     display.print("No Data");
     display.display();
   }  
-  
+
 }
