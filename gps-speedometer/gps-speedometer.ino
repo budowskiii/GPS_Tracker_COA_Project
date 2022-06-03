85d6da7eb7e86c3abef3bce251aa9571f9a17f76
budowskiii <106811443+budowskiii@users.noreply.github.com>
2022-06-03 16:12:15 +0500

    Update gps-speedometer.ino
    Establishing connections to the components.

--- gps-speedometer/gps-speedometer.ino
+++ gps-speedometer/gps-speedometer.ino
@@ -1,18 +1,18 @@
-#include <Wire.h>
-#include <Adafruit_SSD1306.h>
-#include <TinyGPS++.h>
+void setup() {
+  Serial.begin(115200);
+  //Begin serial communication Arduino IDE (Serial Monitor)
 
+  //Begin serial communication Neo6mGPS
+  neogps.begin(9600, SERIAL_8N1, RXD2, TXD2);
+  
+  // SSD1306_SWITCHCAPVCC = generate display voltage from 3.3V internally
+  if(!display.begin(SSD1306_SWITCHCAPVCC, SCREEN_ADDRESS)) {
+    Serial.println(F("SSD1306 allocation failed"));
+    for(;;); // Don't proceed, loop forever
+  }
 
-#define SCREEN_WIDTH 128 // OLED display width, in pixels
-#define SCREEN_HEIGHT 64 // OLED display height, in pixels
+  display.clearDisplay();
+  display.display();
+  delay(2000);
 
-//On ESP32: GPIO-21(SDA), GPIO-22(SCL)
-#define OLED_RESET -1 //Reset pin # (or -1 if sharing Arduino reset pin)
-#define SCREEN_ADDRESS 0x3C //See datasheet for Address
-Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET);
-
-#define RXD2 16
-#define TXD2 17
-HardwareSerial neogps(1);
-
-TinyGPSPlus gps;
+}
